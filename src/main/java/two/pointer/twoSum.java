f0b1a45b64092aa5084f3345d4e13bcc152e0979
shivam798 <shivambghl2000@gmail.com>
2024-03-28 23:39:20 +0530

    Two sum problem added

--- src/main/java/two/pointer/twoSum.java
+++ src/main/java/two/pointer/twoSum.java
@@ -1,7 +1,26 @@
 package two.pointer;
 
+import java.util.Arrays;
+
 public class twoSum {
     public static void main(String[] args) {
-
+        int [] arr = {0, 5, 2, 11};
+        int sum=10;
+        System.out.println(Arrays.toString(helper(arr,sum)));
+    }
+    public static int[] helper(int[] arr,int sum){
+        int start =0;
+        int end=arr.length-1;
+        while(start<end){
+            int total=arr[start]+arr[end];
+            if(total==sum){
+                return new int[]{start,end};
+            } else if (total>sum) {
+                end-=1;
+            }else{
+                start+=1;
+            }
+        }
+        return new int[]{};
     }
 }
