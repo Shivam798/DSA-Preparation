5089fc9528c23256147c7b31418943461ba94abf
shivam798 <shivambghl2000@gmail.com>
2024-07-23 00:27:28 +0530

    Reverse and ZigZag added

---
+++ src/main/java/BFS/ReverseOrder.java
@@ -0,0 +1,39 @@
+package BFS;
+import java.util.*;
+
+public class ReverseOrder {
+    public static void main(String[] args) {
+        TreeNode root =new TreeNode(1);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(4);
+        root.left.right=new TreeNode(5);
+        root.right.left=new TreeNode(6);
+        root.right.right=new TreeNode(7);
+        List<List<Integer>> res = helper(root);
+        System.out.println(res);
+    }
+    private static List<List<Integer>> helper(TreeNode root ){
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        Queue<TreeNode> q= new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int length = q.size();
+            List<Integer> temp = new ArrayList<>();
+            for(int i=0;i<length;i++){
+                TreeNode curNode = q.poll();
+                if(curNode.left!=null){
+                    TreeNode left = curNode.left;
+                    q.add(left);
+                }
+                if(curNode.right!=null){
+                    TreeNode right = curNode.right;
+                    q.add(right);
+                }
+                temp.add(curNode.val);
+            }
+            res.addFirst(temp);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/BFS/Zigzag.java
@@ -0,0 +1,47 @@
+package BFS;
+import com.sun.source.tree.Tree;
+
+import java.util.*;
+
+import static java.util.Collections.*;
+
+public class Zigzag {
+    public static void main(String[] args) {
+        TreeNode root =new TreeNode(1);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(4);
+        root.left.right=new TreeNode(5);
+        root.right.left=new TreeNode(6);
+        root.right.right=new TreeNode(7);
+        List<List<Integer>> res = helper(root);
+        System.out.println(res);
+    }
+    private static List<List<Integer>> helper (TreeNode root){
+        List<List<Integer>> res= new ArrayList<>();
+        Queue<TreeNode> q= new LinkedList<>();
+        q.add(root);
+        boolean flag =true;
+        while(!q.isEmpty()){
+            int length = q.size();
+            LinkedList<Integer> temp = new LinkedList<>();
+            for(int i =0 ;i<length;i++){
+                TreeNode curNode = q.poll();
+                if(flag){
+                    temp.addLast(curNode.val);
+                }else{
+                    temp.addFirst(curNode.val);
+                }
+                if(curNode.left!=null){
+                    q.add(curNode.left);
+                }
+                if(curNode.right!=null){
+                    q.add(curNode.right);
+                }
+            }
+            res.add(temp);
+            flag=!flag;
+        }
+        return res;
+    }
+}
